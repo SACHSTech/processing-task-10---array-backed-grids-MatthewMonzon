d2a0416d9a1abf1adaa5a74f3507c9ad004b597a
MatthewMonzon <matthew.monzon23@ycdsbk12.ca>
2022-05-30 00:17:32 -0400

    drew grid

--- Sketch.java
+++ Sketch.java
@@ -1,13 +1,23 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
+	int CELL_WIDTH = 20;
+  int CELL_HEIGHT = 20;
+  int MARGIN = 5;
+  int ROW_COUNT = 10;
+  int COLUMN_COUNT = 10;
+  int SCREEN_WIDTH = (CELL_WIDTH + MARGIN) * COLUMN_COUNT + MARGIN;
+  int SCREEN_HEIGHT = (CELL_HEIGHT + MARGIN) * ROW_COUNT + MARGIN;
+
+  int[][] intGrid = new int[ROW_COUNT][COLUMN_COUNT];
+
+  int cellsSelected;
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
     // put your size call here
-    size(500, 600);
+    size(SCREEN_WIDTH, SCREEN_HEIGHT);
   }
 
   /** 
@@ -15,15 +25,35 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(255,255,255);
+    background(0,0,0);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
+
+   //draw grid
   public void draw() {
-	  
+	  for (int column = 0; column < COLUMN_COUNT; column++) {
+      for (int row = 0; row < ROW_COUNT; row++) {
+
+        //if array is equal to 1 set color to green
+        if (intGrid[row][column] == 1) {
+          fill(0, 255, 0);
+          rect(MARGIN + (column * (CELL_WIDTH + MARGIN)), MARGIN + (row * (CELL_HEIGHT + MARGIN)), CELL_WIDTH, CELL_HEIGHT);
+       
+        }
+
+        else {
+          fill(255, 255, 255);
+          rect(MARGIN + (column * (CELL_WIDTH + MARGIN)), MARGIN + (row * (CELL_HEIGHT + MARGIN)), CELL_WIDTH, CELL_HEIGHT);
+        }
+      }
+    }
   }
+
+
+  
 
   public void mousePressed(){
     
